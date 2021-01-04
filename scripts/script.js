0825c79c9e686109ae7c679bfe9393ef9dc7c74b
Martijn R <m.reeuwijk@live.nl>
2021-01-04 12:03:53 +0100

    update
    Js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+console.log("Hi")
+
+
+
+
+document.getElementsByClassName("clickthis")[0].addEventListener("click", Showdata);
+document.getElementsByClassName("fakeMenu")[0].addEventListener("click", OpenNav);
+document.querySelectorAll(".overlayNav > div")[0].addEventListener("click", OpenNav);
+
+function Showdata() {
+    document.getElementsByClassName("houseDetailsHidden")[0].classList.toggle("hidden");
+    document.getElementsByClassName("summery")[0].classList.toggle("noAfter");
+}
+
+function OpenNav() {
+    document.getElementsByClassName("overlayNav")[0].classList.toggle("slideInTop");
+}
